08e33f478c5b905d4b2ed83f4b9fffc68099eed3
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-06-13 11:40:46 +0200

    Add files via upload

---
+++ ponavljanje2/ponavljanje2/KlasaD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje2
+{
+    class KlasaD
+    {
+        public bool Suprotno(bool b)
+        {
+            b = !true;
+
+            Console.WriteLine("Suprotno od true je: " + b);
+
+            return b;
+        }
+
+        public int Suprotno(int a)
+        {
+            a = (-1) * a;
+
+            Console.WriteLine("Broj suprotan od 50 je: " + a);
+
+            return a;
+        }
+    }
+}
---
+++ ponavljanje2/ponavljanje2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaD objekt = new KlasaD();
+
+            int a = 50;
+            bool b = true;
+
+            int minus = objekt.Suprotno(a);
+
+            bool trueilifalse = objekt.Suprotno(b);
+
+
+            Console.ReadKey();
+        }
+    }
+}
